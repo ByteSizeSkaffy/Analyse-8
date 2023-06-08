da4e367d90acdb946bcbde4659aa4c8f145c6090
TheBulletBot <41111591+TheBulletBot@users.noreply.github.com>
2023-06-08 22:56:35 +0200

    run fix

--- Logging.py
+++ Logging.py
@@ -3,6 +3,5 @@ import datetime
 def logLoginAttempt(Username, passed, sussy, additionalInfo=""):
     with open('log.txt', 'a') as sys.stdout:
         print(f'{Username} logged in on {datetime.datetime.now()}, which was ' +( "Successful" if passed else "unsuccessful")+("| Warning! possible suspicious activity" if sussy else "")+". Additional info: " + (additionalInfo if additionalInfo != "" else "none"))
-    exit()
-
+    sys.stdout= sys.__stdout__
 logLoginAttempt("Username", True, False, additionalInfo="dipshit")
